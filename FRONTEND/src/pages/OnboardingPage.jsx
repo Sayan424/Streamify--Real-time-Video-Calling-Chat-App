9dd8bde10eb55615f95d3cc487189642c1defc07
Sayan424 <banerjeesayan180@gmail.com>
2025-09-12 16:46:13 +0530

    corrected onboarding

--- FRONTEND/src/pages/OnboardingPage.jsx
+++ FRONTEND/src/pages/OnboardingPage.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import useAuthUser from "../../hooks/useAuthUser.js";
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { completeOnboarding } from "../lib/api.js";
+import { CameraIcon, ShuffleIcon } from "lucide-react";
 
 const OnboardingPage = () => {
-  return <div>OnboardingPage</div>;
+  const { authUser } = useAuthUser();
+  const queryClient = useQueryClient();
+
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+
+  const { mutate: onboardingMutation, isPending } = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () => {
+      toast.success("Profile onboarded successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onboardingMutation(formState);
+  };
+
+  const handleRandomAvatar = () => {};
+
+  return (
+    <div
+      className="min-h-screen bg-base-100 flex items-center justify-center p-4"
+      data-theme="forest"
+    >
+      <div className="card bg-base-200 w-full max-w-3xl shadow-bgshadow">
+        <div className="card-body p-6 sm:p-8">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">
+            Complete Your Profile
+          </h1>
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            {/* Profile pic container */}
+            <div className="flex flex-col items-center justify-center space-y-4">
+              {/* image preview */}
+              <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                {formState.profilePic ? (
+                  <img
+                    src={formState.profilePic}
+                    alt="Profile preview"
+                    className="w-full h-full object-cover"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center h-full">
+                    <CameraIcon className="size-12 text-baseColor opacity-40" />
+                  </div>
+                )}
+              </div>
+              {/* Generate Random Avatar Btn */}
+              <div className="flex items-center gap-2">
+                <button
+                  type="button"
+                  onClick={handleRandomAvatar}
+                  className="bg-avatarButton font-bold py-2 px-4 rounded-xl flex hover:bg-opacity-90  w-full text-[#000] hover:scale-110 transition-all duration-100 "
+                >
+                  <ShuffleIcon className=" size-5 mr-2 self-center" />
+                  Generate Random Avatar
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OnboardingPage;
