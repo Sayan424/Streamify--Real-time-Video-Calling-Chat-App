aba69cc04de608d00833e7451d3c0d78a7387f14
Sayan424 <banerjeesayan180@gmail.com>
2025-09-12 20:18:14 +0530

    created the onboarding page

--- FRONTEND/src/pages/OnboardingPage.jsx
+++ FRONTEND/src/pages/OnboardingPage.jsx
@@ -8,6 +8,7 @@ import {
 import toast from "react-hot-toast";
 import { completeOnboarding } from "../lib/api.js";
 import { CameraIcon, ShuffleIcon } from "lucide-react";
+import { LANGUAGES } from "../constants/index.js";
 
 const OnboardingPage = () => {
   const { authUser } = useAuthUser();
@@ -75,6 +76,89 @@ const OnboardingPage = () => {
                   <ShuffleIcon className=" size-5 mr-2 self-center" />
                   Generate Random Avatar
                 </button>
+              </div>
+            </div>
+            {/* full name */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text"> Full Name </span>
+              </label>
+              <input
+                type="text"
+                name="fullName"
+                value={formState.fullName}
+                onChange={(e) =>
+                  setFormState({ ...formState, fullName: e.target.value })
+                }
+                className="input input-bordered w-full"
+                placeholder="Your Full Name"
+              />
+            </div>
+            {/* /bio input ellement */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Bio</span>
+              </label>
+              <textarea
+                name="bio"
+                value={formState.bio}
+                onChange={(e) =>
+                  setFormState({ ...formState, bio: e.target.value })
+                }
+                className="textarea textarea-bordered h-32"
+                placeholder="Tell others about yourself and your language learning goles...."
+              />
+            </div>
+
+            {/* languages */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {/* native language */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Native Language</span>
+                </label>
+                <select
+                  name="nativeLanguage"
+                  value={formState.nativeLanguage}
+                  onChange={(e) =>
+                    setFormState({
+                      ...formState,
+                      nativeLanguage: e.target.value,
+                    })
+                  }
+                  className="select select-bordered w-full"
+                >
+                  <option value="">Select your Native Language</option>
+                  {LANGUAGES.map((lang) => (
+                    <option key={`native-${lang}`} value={lang.toLowerCase()}>
+                      {lang}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              {/* learning language */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Learning Language</span>
+                </label>
+                <select
+                  name="learningLanguage"
+                  value={formState.learningLanguage}
+                  onChange={(e) =>
+                    setFormState({
+                      ...formState,
+                      learningLanguage: e.target.value,
+                    })
+                  }
+                  className="select select-bordered w-full"
+                >
+                  <option value="">Select your Learning Language</option>
+                  {LANGUAGES.map((lang) => (
+                    <option key={`learning-${lang}`} value={lang.toLowerCase()}>
+                      {lang}
+                    </option>
+                  ))}
+                </select>
               </div>
             </div>
           </form>
