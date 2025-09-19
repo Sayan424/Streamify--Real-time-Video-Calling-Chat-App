17bbf6931753a5c758ec591e9cb7df1062906a7b
Sayan424 <banerjeesayan180@gmail.com>
2025-09-20 00:44:04 +0530

    sidebar

--- FRONTEND/src/components/Sidebar.jsx
+++ FRONTEND/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 3;
 import useAuthUser from "../hooks/useAuthUser.js";
 import { Link, useLocation } from "react-router";
-import { Webhook } from "lucide-react";
+import { HomeIcon, Webhook } from "lucide-react";
 
 const Sidebar = () => {
   const { authUser } = useAuthUser();
@@ -11,8 +11,8 @@ const Sidebar = () => {
   const currentPath = location.pathname;
 
   return (
-    <aside className="w-64 bg-base-200 hidden lg:flex flex-col h-screen sticky top-0  shadow-bgshadow">
-      <div className="p-5 border-b border-base-300 shadow-bgshadow">
+    <aside className="w-64 bg-base-200 hidden lg:flex flex-col h-screen sticky top-0 border-r border-baseColor">
+      <div className="p-5 border-b border-baseColor">
         <Link to="/" className="flex items-center gap-2.5">
           <Webhook className="size-9 text-baseColor" />
           <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-baseColor to-gradient tracking-wider">
@@ -20,6 +20,17 @@ const Sidebar = () => {
           </span>
         </Link>
       </div>
+      <nav className="flex-1 p-4 space-y-1">
+        <Link
+          to="/"
+          className={`btn btn-ghost justify-start w-full gap-3 normal-case ${
+            currentPath === "/" ? "btn-active" : ""
+          }`}
+        >
+          <HomeIcon className="size-5 text-base-content opacity-70" />
+          <span>Home</span>
+        </Link>
+      </nav>
     </aside>
   );
 };
