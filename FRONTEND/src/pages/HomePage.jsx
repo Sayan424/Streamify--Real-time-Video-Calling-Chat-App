de5a3883f5224721ef77e036aa2252579d9c7a44
Sayan424 <banerjeesayan180@gmail.com>
2025-09-22 01:02:20 +0530

    improved

--- FRONTEND/src/pages/HomePage.jsx
+++ FRONTEND/src/pages/HomePage.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import {
+  getOutgoingFriendReqs,
+  getRecommendedUsers,
+  getUserFriends,
+  sendFriendRequest,
+} from "../lib/api";
 
 const HomePage = () => {
+  const queryClient = useQueryClient();
+  const { outgoingRequestsIds, setOutgoingRequestsIds } = useState([]);
+
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
+
+  const { data: recommendedUsers = [], isLoading: loadingUsers } = useQuery({
+    queryKey: ["users"],
+    queryFn: getRecommendedUsers,
+  });
+
+  const { data: outgoingFriendReqs } = useQuery({
+    queryKey: ["outgoingFriendReqs"],
+    queryFn: getOutgoingFriendReqs,
+  });
+
+  const { mutate: sendRequestMutation, isPending } = useMutation({
+    mutationFn: sendFriendRequest,
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["outgoingFriendReqs"] }),
+  });
+
   return <div>HomePage</div>;
 };
 
