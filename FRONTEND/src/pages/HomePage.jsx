5a381656c7df69560fce13785e233c9836dcc2b9
Sayan424 <banerjeesayan180@gmail.com>
2025-09-25 22:55:18 +0530

    created home page

--- FRONTEND/src/pages/HomePage.jsx
+++ FRONTEND/src/pages/HomePage.jsx
@@ -1,15 +1,19 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   getOutgoingFriendReqs,
   getRecommendedUsers,
   getUserFriends,
   sendFriendRequest,
 } from "../lib/api";
+import { Link } from "react-router";
+import { UsersIcon } from "lucide-react";
+import FriendCard from "../components/FriendCard";
+import NoFriendsFound from "../components/NoFriendsFound";
 
 const HomePage = () => {
   const queryClient = useQueryClient();
-  const { outgoingRequestsIds, setOutgoingRequestsIds } = useState([]);
+  const { outgoingRequestsIds, setOutgoingRequestsIds } = useState([new Set()]);
 
   const { data: friends = [], isLoading: loadingFriends } = useQuery({
     queryKey: ["friends"],
@@ -32,7 +36,60 @@ const HomePage = () => {
       queryClient.invalidateQueries({ queryKey: ["outgoingFriendReqs"] }),
   });
 
-  return <div>HomePage</div>;
+  useEffect(() => {
+    const outgoingIds = new Set();
+    if (outgoingFriendReqs && outgoingFriendReqs.length > 0) {
+      outgoingFriendReqs.forEach((req) => {
+        outgoingIds.add(req.id);
+      });
+      setOutgoingRequestsIds(outgoingIds);
+    }
+  }, [outgoingFriendReqs]);
+
+  return (
+    <div className="p-4 sm:p-6 lg:p-8">
+      <div className="container mx-auto space-y-10">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+          <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
+            Your Friends
+          </h2>
+          {/* in the future  think it need to change to /friends from /notifications */}
+          <Link to="/friends" className="btn btn-outline btn-sm">
+            <UsersIcon className="mr-2 size-4" />
+            Friend Requests
+          </Link>
+        </div>
+        {loadingFriends ? (
+          <div className="flex justify-center py-12">
+            <span className="loading loading-spinner loading-lg" />
+          </div>
+        ) : friends.length === 0 ? (
+          <NoFriendsFound />
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {friends.map((friend) => (
+              <FriendCard key={friend.id} friend={friend} />
+            ))}
+          </div>
+        )}
+
+        <section>
+          <div className="mb-6 sm:mb-8">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+              <div>
+                <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
+                  Meet New Friends
+                </h2>
+                <p className="opacity-70">
+                  Converse With More People from Your Profile
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
