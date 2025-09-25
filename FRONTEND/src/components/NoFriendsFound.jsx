b54cf1dbbdc294a04b76a7c3809985f288cfa869
Sayan424 <banerjeesayan180@gmail.com>
2025-09-25 22:55:55 +0530

    created home page

---
+++ FRONTEND/src/components/NoFriendsFound.jsx
@@ -0,0 +1,12 @@
+const NoFriendsFound = () => {
+  return (
+    <div className="card bg-baseColor/60 p-6 text-center">
+      <h3 className="font-semibold text-lg mb-2">No friends found.</h3>
+      <p className="text-base-content opacity-70">
+        Connect with people by sending them friend requests!
+      </p>
+    </div>
+  );
+};
+
+export default NoFriendsFound;
