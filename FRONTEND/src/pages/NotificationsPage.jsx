4c36060ad55b13117a0cb5dc1d5c7348c9ee4cf9
Sayan424 <banerjeesayan180@gmail.com>
2025-10-08 23:15:28 +0530

    home page done

--- FRONTEND/src/pages/NotificationsPage.jsx
+++ FRONTEND/src/pages/NotificationsPage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { acceptFriendRequest, getFriendRequests } from "../lib/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const NotificationsPage = () => {
-  return <div>NotificationsPage</div>;
+  const queryClient = useQueryClient();
+  const { data: friendRequests, isLoading } = useQuery({
+    queryKey: ["friendRequests"],
+    queryFn: getFriendRequests,
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["friendRequests"] }),
+  });
+
+  const { mutate: acceptRequestMutation, isPending } = useMutation({
+    mutationFn: acceptFriendRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["friendRequests"] });
+      queryClient.invalidateQueries({ queryKey: ["friends"] });
+    },
+  });
+
+  const incomingRequests = friendRequests?.incomingReqs || [];
+  const acceptedRequests = friendRequests?.acceptedReqs || [];
+
+  return (
+    <div className="p-4 sm:p-6 lg:p-8">
+      <div className="container mx-auto max-w-4xl space-y-8">
+        <h1 className="text-2xl sm:text-3xl font-bold tracking-tight mb-6">
+          Notifications
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default NotificationsPage;
