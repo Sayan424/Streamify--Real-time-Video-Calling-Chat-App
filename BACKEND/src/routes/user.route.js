c6bbe6c37c3ca8fdd541ed72048d1c100f7a7e07
Sayan424 <banerjeesayan180@gmail.com>
2025-10-08 23:13:43 +0530

    home page done

--- BACKEND/src/routes/user.route.js
+++ BACKEND/src/routes/user.route.js
@@ -19,7 +19,7 @@ router.get("/friends", getMyFriends);
 router.post("/friend-request/:id", sendFriendRequest);
 router.put("/friend-request/:id/accept", acceptFriendRequest);
 
-router.get("/friend-request", getFriendRequest);
-router.get("/outgoing-friend-request", getOutgoingFriendRequest);
+router.get("/friend-requests", getFriendRequest);
+router.get("/outgoing-friend-requests", getOutgoingFriendRequest);
 
 export default router;
